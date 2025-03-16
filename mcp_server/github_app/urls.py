248c0e666f6de29ee59a1d29bf0df4f13a0680a1
Stormbreaker <aliasgarabbas42@gmail.com>
2025-03-16 22:01:11 +0530

    mcp-server-added

--- mcp_server/github_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import GitHubRepositoryView
-
-urlpatterns = [
-
-    path('repo/<str:owner>/<str:repo_name>/', GitHubRepositoryView.as_view(), name='github-repo'),
-]
